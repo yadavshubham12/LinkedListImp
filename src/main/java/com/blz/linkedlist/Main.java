0f37143af73354bab9451799757ba351056e361e
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-20 13:51:34 +0530

    ADDED: Simple linkedlist added

--- src/main/java/com/blz/linkedlist/Main.java
+++ src/main/java/com/blz/linkedlist/Main.java
@@ -1,2 +1,11 @@
-package com.blz.linkedlist;public class Main {
+package com.blz.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addNode(56);
+        list.addNode(30);
+        list.addNode(70);
+        list.printList();
+    }
 }
--- src/main/java/com/blz/linkedlist/Node.java
+++ src/main/java/com/blz/linkedlist/Node.java
@@ -1,2 +1,27 @@
-package com.blz.linkedlist;public class Node {
+package com.blz.linkedlist;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = next;
+    }
+}
+class LinkedList {
+    Node head;
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+    public void printList(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        }
+    }
 }
